740ad3650e27d17d99bc0e3e8c33d599eeff87a4
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-12-12 21:08:29 -0500

    Added ItemView with the rudimentary HTML template, that is extended from the Core template.

--- menu/urls.py
+++ menu/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from menu import views
+from menu.views import ItemView
 
 app_name = 'menu'
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name=''), name="all"),
+    path('', views.ItemView.as_view(), name="all"),
     # path('/<int:menu_id>/', views.menu, name='item'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,14 @@
+from tkinter import Menu
+
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+
+from menu.models import Item
+
 
 # Create your views here.
+
+class ItemView(ListView):
+    model = Item
+
