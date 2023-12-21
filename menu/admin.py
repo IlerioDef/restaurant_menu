3a229ce4892d8019abb8c9e5ebf8f85e9c73a131
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-12-20 22:29:17 -0500

    added several models: Order Chef Table

--- menu/admin.py
+++ menu/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 
-from menu.models import Allergen, Category, Item
+from menu.models import Allergen, Category, Item, Table, Order, Chef
 
 admin.site.register(Allergen)
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(Chef)
--- menu/models.py
+++ menu/models.py
@@ -43,3 +43,74 @@ class Item(models.Model):
             return allergens
 
         return None
+
+
+class Chef(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+    """
+    Table Model, by default the number of tables is 13. You may change the number of tables manually
+    """
+    def get_table_names():
+        table_choices = []
+        for x in range(1, 14):
+            table_choices.append((f"{x}", f"Table #{x}"))
+        return table_choices
+
+    def get_table_status():
+        status_options = {
+            ("O","Open"),
+            ("C","Closed"),
+            ('R', "Reserved")
+
+        }
+        return status_options
+
+    name = models.CharField(max_length=10,
+                            choices=get_table_names())
+    status = models.CharField(max_length=3,
+                              choices=get_table_status())
+
+    def __str__(self):
+        return f"Table #{self.name}"
+
+class Order(models.Model):
+    PENDING = "P"
+    ACCEPTED = "A"
+    PROCESSING = "PR"
+    DONE = "D"
+    CANCELLED = "C"
+
+    ORDER_STATUS_CHOICES = {
+        PENDING: "Pending",
+        ACCEPTED: "Accepted",
+        PROCESSING: "Processing",
+        DONE: "Done",
+        CANCELLED: "Cancelled",
+    }
+    id = models.AutoField
+    table = models.CharField(
+        max_length=2,
+        choices=Table.get_table_names(),
+        default=PENDING
+    )
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)  # What exactly is under preparation
+    quantity = models.IntegerField()  # How many items are under preparation
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # How much does it cost. WARNING TODO! This should be
+    # immutable after the order is done.
+    order_total = models.DecimalField(max_digits=10, decimal_places=2)  # Total price of the order
+    status = models.CharField(
+        max_length=2,
+        choices=ORDER_STATUS_CHOICES,
+        default=PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    assigned_to = models.ForeignKey(Chef, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order # {self.id}"
